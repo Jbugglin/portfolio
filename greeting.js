eda9353e25dbcc006cb03405ae3c08fe54fa4729
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-03-15 15:47:20 -0400

    Update
    Rolled back the glassmorphism and just sticking to a basic setup for now

--- greeting.js
+++ greeting.js
@@ -3,20 +3,18 @@
  *  will change the greeting of the webpage 
  *  accordingly. (Morning, Afternoon, and Evening).
  */
-const now = new Date();
-const hours = now.getHours();
-let timeGreeting = "";
+function timeGreet(){
+    const now = new Date();
+    const hours = now.getHours();
+    let timeGreeting = "";
 
-if (hours < 12) {
-    greeting = "Morning!";
-    document.body.style.backgroundImage = "linear-gradient(to top, pink, yellow)";
-} else if (hours < 18) {
-    greeting = "Afternoon!";
-    document.body.style.backgroundImage = "linear-gradient(to top, #a8dbcb, #a8c2db 25%)";
-} else {
-    greeting = "Evening!";
-    document.body.style.backgroundImage = "linear-gradient(to top right, red, purple, blue)";
-    document.body.style.color = "white"
+    if (hours < 12) {
+        greeting = "Morning!";
+    } else if (hours < 18) {
+        greeting = "Afternoon!";
+    } else {
+        greeting = "Evening!";
+    }
+
+    document.getElementById("timeGreet").textContent = greeting;
 }
-
-document.getElementById("timeGreet").textContent = greeting;
