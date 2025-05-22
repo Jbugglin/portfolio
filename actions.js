9957f74ebe3977646287b370e87e92f5f1c42790
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-05-21 22:49:04 -0400

    Update
    I played around with some styling of the links in the navbar in mobile vp. I also added in a time-sensitive greeting to the landing page (index)

--- actions.js
+++ actions.js
@@ -1,5 +1,6 @@
-//This section will give our navbar click-logic.
-//Declarations...
+/**
+ * Nav-bar click logic. 
+ */
 let openHam = document.querySelector('#openHam');
 let closeHam = document.querySelector('#closeHam');
 let navItems = document.querySelector('#nav-items');
@@ -14,3 +15,24 @@ const hamburgerEvent = (navigation, close, open) => {
 //Attach event listeners for the clicking of the open and close
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
 closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+
+
+/**
+ *  Greeting, I want to use this to adjust the greeting on the landing
+ *      page based upon the time of the day. 
+ */
+function homeGreeting() {
+    let span = document.getElementById("greeting");
+    let hours = new Date();
+    console.log(hours);
+    if (hours < 12) {
+        //console.log("Morning");
+        span.innerText = "Morning";
+    } else if (hours >= 12 && hours < 18 ){
+        //console.log("Afternoon");
+        span.innerText = "Afternoon";
+    } else {
+        //console.log("Evening");
+        span.innerText = "Evening";
+    }
+}
