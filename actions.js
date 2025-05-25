8d8c47bc666f061577750f6cfe0e52d17eda7989
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-05-25 00:00:44 -0400

    Restart
    Had issues with the last iteration of my portfolio, the hamburger menu worked on mobile, but failed at larger viewports.

--- actions.js
+++ actions.js
@@ -1,23 +1,4 @@
 /**
- * Nav-bar click logic. 
- */
-let openHam = document.querySelector('#openHam');
-let closeHam = document.querySelector('#closeHam');
-let navItems = document.querySelector('#nav-items');
-
-//Take 3 parameters, and modifies CSS display based on provided values
-const hamburgerEvent = (navigation, close, open) => {
-    navItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
-}
-
-//Attach event listeners for the clicking of the open and close
-openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
-closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-
-
-/**
  *  Greeting, I want to use this to adjust the greeting on the landing
  *      page based upon the time of the day. 
  */
