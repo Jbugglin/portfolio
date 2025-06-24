7b910967e3a407414330f458de72f3fb7e97c2a8
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-06-23 22:01:01 -0400

    Update
    I will have to fix the nav menu closing after clicking on a link to the next section. I also will add more content to the sections. I was playing around with colors in JS

--- actions.js
+++ actions.js
@@ -8,12 +8,21 @@ function homeGreeting() {
     if (hours.getHours() < 12) {
         //console.log("Morning");
         span.innerText = "Morning";
+        document.body.style.backgroundColor = "rgb(244,244,77)";
+        document.getElementById("greeting-container").style.backgroundColor = "rgb(216, 216, 70)";
+        document.getElementById("header").style.backgroundColor = "rgb(216, 216, 70)";
     } else if (hours.getHours() >= 12 && hours.getHours() < 18 ){
         //console.log("Afternoon");
         span.innerText = "Afternoon";
+        document.body.style.backgroundColor = "rgb(206,227,151)";
+        document.getElementById("greeting-container").style.backgroundColor = "rgb(188, 221, 106)";
+        document.getElementById("header").style.backgroundColor = "rgb(188, 221, 106)";
     } else {
         //console.log("Evening");
         span.innerText = "Evening";
+        document.body.style.backgroundColor = "rgb(88,119,146)";
+        document.getElementById("greeting-container").style.backgroundColor = "rgb(140, 154, 167)";
+        document.getElementById("header").style.backgroundColor = "rgb(140, 154, 167)";
     }
 }
 
